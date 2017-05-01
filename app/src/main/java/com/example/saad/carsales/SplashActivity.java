a7921e649b0f054b49d298441c019481ec066d50
Saad <saadbnwhd@gmail.com>
2017-05-01 23:42:19 +0500

    Liquid Button Added on Splash Screen.

--- app/src/main/java/com/example/saad/carsales/SplashActivity.java
+++ app/src/main/java/com/example/saad/carsales/SplashActivity.java
@@ -5,48 +5,72 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.gospelware.liquidbutton.LiquidButton;
+
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash);
 
         final ImageView img_logo=(ImageView) findViewById(R.id.imageView);
-        final TextView title=(TextView) findViewById(R.id.textView);
+        LiquidButton liquidButton = (LiquidButton) findViewById(R.id.button);
 
-        new Handler().postDelayed(new Runnable(){
+        final TextView progress_txt= (TextView) findViewById(R.id.progress_txt);
+        liquidButton.startPour();
+        liquidButton.setAutoPlay(true);
+
+        liquidButton.setPourFinishListener(new LiquidButton.PourFinishListener() {
             @Override
-            public void run() {
-
-                //Logo Visibility toggle
-                Animation fadeIn = new AlphaAnimation(0, 1);
-                fadeIn.setInterpolator(new DecelerateInterpolator()); //add this
-                fadeIn.setDuration(500);
-
-                img_logo.setAnimation(fadeIn);
-                title.setAnimation(fadeIn);
-
-                img_logo.setVisibility(View.VISIBLE);
-                title.setVisibility(View.VISIBLE);
-
-                new Handler().postDelayed(new Runnable(){
-                    @Override
-                    public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
-                        Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
-                        SplashActivity.this.startActivity(mainIntent);
-                        SplashActivity.this.finish();
-                    }
-                }, 1000);
-
+            public void onPourFinish() {
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
             }
-        }, 500);
+
+            @Override
+            public void onProgressUpdate(float progress)
+            {
+                progress_txt.setText(String.format("%.2f", progress * 100) + "%");
+            }
+        });
+        //liquidButton.startPour();
+//        new Handler().postDelayed(new Runnable(){
+//            @Override
+//            public void run() {
+//
+//                //Logo Visibility toggle
+//                Animation fadeIn = new AlphaAnimation(0, 1);
+//                fadeIn.setInterpolator(new DecelerateInterpolator()); //add this
+//                fadeIn.setDuration(500);
+//
+//
+//                new Handler().postDelayed(new Runnable(){
+//                    @Override
+//                    public void run() {
+//                /* Create an Intent that will start the Menu-Activity. */
+//
+//                    }
+//                }, 1000);
+//
+//            }
+//        }, 500);
     }
 }
