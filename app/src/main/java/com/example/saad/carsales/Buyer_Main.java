8d87682943881c528b54a4608c62a1bdd0366ff3
MusabAkram <musabakram16@gmail.com>
2017-05-26 02:07:14 +0500

    ads_approve

--- app/src/main/java/com/example/saad/carsales/Buyer_Main.java
+++ app/src/main/java/com/example/saad/carsales/Buyer_Main.java
@@ -145,7 +145,6 @@ public class Buyer_Main extends AppCompatActivity {
 
                 current.setTitle(dataSnapshot.child("Model").getValue().toString());
                 current.setYear(dataSnapshot.child("Model Year").getValue().toString());
-                current.setCar_owner(dataSnapshot.child("Seller Name").getValue().toString());
                 current.setCar_owner(dataSnapshot.child("Name").getValue().toString());
                 current.setAdd_id(dataSnapshot.getKey().toString());
 
